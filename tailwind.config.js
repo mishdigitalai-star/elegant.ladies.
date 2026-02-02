3dd346129472bdbea75bb48a8da87d5a26d415d6
mishdigitalai-star <256646699+mishdigitalai-star@users.noreply.github.com>
2026-02-02 20:44:37 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'brand-brown': '#493A30',
+        'brand-orange': '#E97413',
+        'brand-green': '#66753A',
+        'brand-gold': '#DEAF76',
+        'brand-ivory': '#FBF7F3',
+      },
+      fontFamily: {
+        'serif': ['Playfair Display', 'serif'],
+        'sans': ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
